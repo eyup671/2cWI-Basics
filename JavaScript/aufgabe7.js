c8dbd1d08b768ff5b56bbf48d42dd922c0e516d8
eyup671 <94118800+eyup671@users.noreply.github.com>
2022-03-17 13:46:26 +0100

    switch

---
+++ JavaScript/aufgabe7.js
@@ -0,0 +1,22 @@
+let random = Math.round(Math.random() * 5) + 5;
+console.log(random);
+
+switch (random) {
+    case 10:
+        console.log("Ten");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 5:
+        console.log("Five");
+        break;}
