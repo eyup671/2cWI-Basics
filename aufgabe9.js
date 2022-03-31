9d987738ce3a0bb4f803c83f299fec79b2e91262
eyup671 <94118800+eyup671@users.noreply.github.com>
2022-03-31 13:21:04 +0200

    9

---
+++ aufgabe9.js
@@ -0,0 +1,15 @@
+for (let count = 0; count < 4; count++) {
+    console.log("O");
+}
+
+for (let count = 0; count < 4; count++) {
+    console.log("X");
+}
+for (let count = 0; count < 4; count++) {
+    console.log("O");
+}
+
+for (let count = 0; count < 4; count++) {
+    console.log("X");
+}
+
