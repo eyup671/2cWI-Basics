c6f5d49b9521035692b52ae830d191ca7e5a74ef
eyup671 <94118800+eyup671@users.noreply.github.com>
2022-04-20 15:57:50 +0200

    aa's

---
+++ JavaScript/aufgabe10.js
@@ -0,0 +1,23 @@
+var resadd = add(5, 31);
+
+function add(a, b) {
+    var result = a + b;
+    return result;
+}
+console.log(resadd);
+
+var ressub = subtract(51, 41);
+
+function subtract(a, b) {
+    var result = a - b;
+    return result;
+}
+console.log(ressub);
+
+var resmul = multiply(15, 3);
+
+function multiply(a, b) {
+    var result = a * b;
+    return result;
+}
+console.log(resmul);
---
+++ JavaScript/aufgabe9.js
@@ -0,0 +1,10 @@
+for (let count = 0; count < 4; count++) {
+    console.log("O");
+}
+
+for (let count = 0; count < 4; count++) {
+    console.log("X");
+}
+for (let count = 0; count < 4; count++) {
+    console.log("O");
+}
