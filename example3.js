923c8ac9a13daa9bd1b2eed2dcda6b8fd5e7f9da
eyup671 <94118800+eyup671@users.noreply.github.com>
2022-06-09 14:35:15 +0200

    Add files via upload

---
+++ example3.js
@@ -0,0 +1,12 @@
+console.log("Zufallszahlen:");
+
+for(let random = 0; random < 100; random +=10) {
+    let random = Math.random() * 100;
+let randomAsInteger = Math.round(random);
+console.log(randomAsInteger);
+}
+
+let randomAsInteger = Math.round(random);
+let summe = randomAsInteger;
+
+console.log(summe);
